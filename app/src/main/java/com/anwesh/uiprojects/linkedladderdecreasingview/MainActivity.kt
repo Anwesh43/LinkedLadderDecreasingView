d90683c0df26f622e1b2ad57c78c507bbeb1a6c5
Anwesh43 <anweshthecool0@gmail.com>
2018-08-23 03:38:26 +0530

    adding LadderDecreasingView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedladderdecreasingview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedladderdecreasingview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedladderdecreasingview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.ladderdecreasingview.LadderDecreasingView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LadderDecreasingView.create(this)
     }
 }
