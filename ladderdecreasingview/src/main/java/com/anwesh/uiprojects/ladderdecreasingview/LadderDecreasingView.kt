268facc32c7602d7e14e95b02175906528c0bea2
Anwesh43 <anweshthecool0@gmail.com>
2018-08-23 02:11:16 +0530

    importing libraries and defining nodes

--- ladderdecreasingview/src/main/java/com/anwesh/uiprojects/ladderdecreasingview/LadderDecreasingView.kt
+++ ladderdecreasingview/src/main/java/com/anwesh/uiprojects/ladderdecreasingview/LadderDecreasingView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.ladderdecreasingview
 /**
  * Created by anweshmishra on 23/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.content.Context
+
+val nodes : Int = 5
+
